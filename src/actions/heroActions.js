68ef3feac5b921c927911415be341ae46193d04e
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-09 18:29:03 -0600

    remove console.log calls

--- src/actions/heroActions.js
+++ src/actions/heroActions.js
@@ -3,15 +3,12 @@ import axios from "axios";
 export const getHero = (hero) => {
   return (dispatch, getState) => {
     dispatch({ type: "FETCHING_HERO_START" });
-    console.log(hero);
     axios
       .get(`https://hotsapi.net/api/v1/heroes/${hero}`)
       .then((res) => {
-        console.log(res.data);
         dispatch({ type: "FETCHING_HERO_SUCCESS", payload: res.data });
       })
       .catch((err) => {
-        console.log(err);
         dispatch({
           type: "FETCHING_HERO_ERROR",
           payload: { err },
